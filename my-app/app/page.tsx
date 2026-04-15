fed3e76f8216dc7e8dedb68f653a3d6909108e23
drini <drininazeraj14@gmail.com>
2026-04-15 15:34:21 +0200

    icona

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Login from "./auth/login/page";
 
-export default function Home() {
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/auth/login");
+  }, []);
+
   return <Login />;
 }
