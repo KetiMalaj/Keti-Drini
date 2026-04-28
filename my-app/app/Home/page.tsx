dc09704d6078cf98eb025ff0824de76c00eafdb7
Keti Malaj <Keti>
2026-04-28 16:41:41 +0200

    addstudent

--- my-app/app/Home/page.tsx
+++ my-app/app/Home/page.tsx
@@ -1,7 +1,61 @@
-export default function HomePage() {
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [showForm, setShowForm] = useState(false);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    axios.post(`/api/student`, {
+        name,
+        surname,
+      })
+      .then(function (response) {
+        console.log(response.data);
+
+        setName("");
+        setSurname("");
+        setShowForm(false);
+      });
+  };
+
   return (
-    <div>
-      <h1>Hello! You are logged in.</h1>
+    <div className="p-10">
+      <button
+        onClick={() => setShowForm(true)}
+        className="bg-violet-800 text-white px-4 py-2 rounded"
+      >
+        Add Student
+      </button>
+
+      {showForm && (
+        <form onSubmit={handleSubmit} className="mt-5 space-y-4">
+          <input
+            placeholder="Name"
+            className="border p-2"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            placeholder="Surname"
+            className="border p-2"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="bg-green-600 text-white px-4 py-2 rounded"
+          >
+            Save
+          </button>
+        </form>
+      )}
     </div>
   );
 }
